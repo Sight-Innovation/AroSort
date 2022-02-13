14db7e7cfbfa2c5bafc9907366b7ce4ff2ae650e
Emmanuel <agbavweisaac@gmail.com>
2022-02-13 11:54:48 +0100

    Documented out the algorithm

--- emSort.js
+++ emSort.js
@@ -1,5 +1,23 @@
 /**
- * This populates an an array of a specified size with randomly generated whole numbers given the min and max range as maxNMin
+ *   Description: Sorting algorithm based on hash tables.
+ * 
+ *   Copyright 2022 Emmanuel Agbavwe to Present.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+/**
+ * This populates an an array of a specified size with randomly generated whole numbers given the min and max range as negPos
  * @param {Number} size 
  * @param {Number} negPos 
  * @returns Array<Number>
@@ -11,20 +29,20 @@ const genArray = (size, negPos) => {
     }
     return randomArray;
 };
+// Please note that this takes considerable amount of runtime for large arrays (usually few seconds)
 // To verify the output data in the array run the line below uncomment out and run the line below
-// console.log(genArray(10000000, 1000000));
+// console.log(genArray(10000000, 1000000)); // This will create an array 1 millions indexes big and fill it with whole numbers given the minimum possible and maximum possible values as -1000000 and 1000000 respectively
 
 
 /**
- * EmSort, this sorts an array of whole numbers, be it negative or positive using the concept of hash tables
+ * EmSort, this sorts an array of whole numbers, be it negative or positive using hash keys
  * @param {Array<Number>} array 
  * @returns Array<Number>
  */
 const emSort = (array) => {
     if (array === null || array === undefined) {
-        return array;
-    }
-    if (array.length <= 1) {
+        return [];
+    } else if (array.length <= 1) {
         return array;
     } else {
         const pos = {}, neg = {}, sorted = [];
@@ -33,25 +51,33 @@ const emSort = (array) => {
         for (let index = 0; index < array.length; index++) {
             let element = Number(array[index]);
 
+            // Creating the hash for each number found
             if (element < 0) {
                 element *= -1;
+                // If a duplicate negative number is found, increment its occurrence count
                 if (neg[element]) {
                     neg[element]++;
                 } else {
+                    // If a negative number is found, then its passed into the neg hash table
                     neg[element] = 1;
                 }
                 negLength++;
             } else {
+                // If a duplicate positive number is found, increment its occurrence count
                 if (pos[element]) {
                     pos[element]++;
                 } else {
+                    // If a positive number is found, then its passed into the pos hash table
                     pos[element] = 1;
                 }
             }
         }
 
-
+        // If negative numbers did occur...
         if (negLength) {
+            // Dependent on the fact that the keys in JavaScript are arranged by default, the keys in each hash table are looped through to insert their values into the sorted array
+
+            // Loop through the presorted keys of the neg hash table and assign their values to their rightful sorted position in the sorted array
             for (const numValue in neg) {
                 if (neg[numValue] > 1) {
                     for (let i = 0; i < neg[numValue]; i++) {
@@ -65,18 +91,21 @@ const emSort = (array) => {
             }
         }
 
-        for (const numValue in pos) {
-            if (pos[numValue] > 1) {
-                for (let i = 0; i < pos[numValue]; i++) {
+        // If positive numbers did occur...
+        if (Object.keys(pos).length) {
+            // Loop through the presorted keys of the pos hash table and assign their values to their rightful sorted position in the sorted array
+            for (const numValue in pos) {
+                if (pos[numValue] > 1) {
+                    for (let i = 0; i < pos[numValue]; i++) {
+                        sorted[negLength] = Number(numValue);
+                        negLength++;
+                    }
+                } else {
                     sorted[negLength] = Number(numValue);
                     negLength++;
                 }
-            } else {
-                sorted[negLength] = Number(numValue);
-                negLength++;
             }
         }
-
         return sorted;
     }
 };
@@ -85,4 +114,4 @@ let unsortedArray = genArray(1000000, 1000000);
 
 // console.log('Sorted', emSort(unsortedArray));
 
-emSort(unsortedArray)
+emSort(unsortedArray);
--- emSort.py
+++ emSort.py
@@ -1,17 +1,48 @@
-'''This populates an an array of a specified size with randomly generated whole numbers given the min and max range as maxNMin'''
+'''
+    Description: Sorting algorithm based on hash tables.
+
+    Copyright 2022 Emmanuel Agbavwe to Present.
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+'''
+
 import math
 import random
 
 def genArray(size, neg_pos):
+    '''This populates an an array of a specified size with randomly generated whole numbers given the min and max range as neg_pos
+    :param size: int
+    :neg_pos: int
+    :return: list[int]
+    '''
     randomArray = [0] * size
     for i in range(size):
         randomArray[i] = (int(math.floor(random.randrange(neg_pos*-1,neg_pos+1))))
     return randomArray    
+# Please note that this takes considerable amount of runtime for large arrays (usually few seconds)
+# To verify the output data in the array run the line below uncomment out and run the line below
+# print(genArray(1000000,1000000)) # This will create an array 1 million indexes big and fill it with whole numbers given the minimum possible and maximum possible values as -1000000 and 1000000 respectively
 
-# genArray(1000000,1000000)
 
 def emSort(array):
-    if len(array) <= 1:
+    '''
+    EmSort, this sorts an array of whole numbers, be it negative or positive using hash keys
+    :param array: list[int]
+    :return: list[int]
+    '''
+    if array == None:
+        return []
+    elif len(array) <= 1:
         return array
     else:
         pos = {}
@@ -23,20 +54,29 @@ def emSort(array):
         for i in range(len(array)):
             element = array[i]
 
+            # Creating the hash for each number found
             if element < 0:
                 element *= -1
+                # If a duplicate negative number is found, increment its occurrence count
                 if element in neg:
                     neg[element] += 1
                 else:
+                    # If a negative number is found, then its passed into the neg hash table
                     neg[element] = 1
                 negLength += 1
             else:
+                # If a duplicate positive number is found, increment its occurrence count
                 if element in pos:
-                    pos[element] = pos[element] +1
+                    pos[element] +=1
                 else:
+                    # If a positive number is found, then its passed into the pos hash table
                     pos[element] = 1
 
+        # If negative numbers did occur...
         if negLength > 0:
+            # Considering the fact that the keys in Python are not arranged by default, the sorted() function - which uses the TimSort algorithm - is called to carryout sorting on the keys, then the result is looped through to insert their values into the sortedArray
+
+            # Loop through the sorted keys of the neg hash table and assign their values to their rightful sorted position in the sortedArray
             for numValue in sorted(neg.keys()):
                 if neg[numValue] > 1:
                     for i in range(neg[numValue]):
@@ -46,18 +86,22 @@ def emSort(array):
                     sortedArray[negLength - counter] = int(numValue) * -1
                     counter += 1
         
-        for numValue in sorted(pos.keys()):
-            if pos[numValue] > 1:
-                    for i in range(pos[numValue]):
-                        sortedArray[negLength] = int(numValue)
-                        negLength += 1
-            else:
-                sortedArray[negLength] = int(numValue)
-                negLength += 1
+        # If positive numbers did occur...
+        if len(pos) > 0:
+            # Loop through the sorted keys of the neg hash table and assign their values to their rightful sorted position in the sortedArray
+            for numValue in sorted(pos.keys()):
+                if pos[numValue] > 1:
+                        for i in range(pos[numValue]):
+                            sortedArray[negLength] = int(numValue)
+                            negLength += 1
+                else:
+                    sortedArray[negLength] = int(numValue)
+                    negLength += 1
 
         return sortedArray
 
 unsortedArray = genArray(1000000, 1000000)
 
-# print(emSort(unsortedArray))
+# print('Sorted', emSort(unsortedArray))
+
 emSort(unsortedArray)
